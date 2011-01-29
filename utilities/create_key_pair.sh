dc52eb2e0eaa4d78b6565db9dd3265bae662c8a2
Dan Wendlandt <danwent@gmail.com>
2011-01-29 12:22:08 -0800

    Put files under GPLv2

--- utilities/create_key_pair.sh
+++ utilities/create_key_pair.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#   This file is part of the Perspectives Notary Server
+#
+#   Copyright (C) 2011 Dan Wendlandt
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 exec >&2
 
 if [ "$#" != 2 ] ; then
