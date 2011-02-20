62dbd519dc732ef9ca239ed6925db82ccf1e3722
Dan Wendlandt <danwent@gmail.com>
2011-02-19 19:13:57 -0800

    refactor ssl_scan2.py to handle servers that send multiple handshake protocols in a single record

--- ssl_scan2.py
+++ ssl_scan2.py
@@ -40,8 +40,48 @@ def read_data(s,data_len):
 		if len(buf_str) == data_len:
 			return buf_str
 
-if len(sys.argv) != 3:
-	print >> sys.stderr, "ERROR: usage: <service-id> <notary-db-file>"
+def read_record(s): 
+	rec_start = read_data(sock,5)
+	if len(rec_start) != 5: 
+		print "Error: unable to read start of record"
+		exit(1) 
+
+	(rec_type, ssl_version, tls_version, rec_length) = struct.unpack('!BBBH',rec_start)
+	print "record: type = %s  length = %s" % (rec_type, rec_length)
+	rest_of_rec = read_data(sock,rec_length)
+	if len(rest_of_rec) != rec_length: 
+		print "Error: unable to read full record"
+		exit(1) 
+	return (rec_type, rest_of_rec)
+
+def get_all_handshake_protocols(rec_data):
+	protos = [] 
+	while len(rec_data) > 0: 
+		t, b1,b2,b3 = struct.unpack('!BBBB',rec_data[0:4])
+		l = (b1 << 16) | (b2 << 8) | b3
+		print "handshake protocol of type %s and length %s" % (t,l) 
+		protos.append((t, rec_data[4: 4 + l]))
+		rec_data = rec_data[4 + l:]
+	return protos 
+
+# rfc 2246 says the server cert if the first one
+# in the chain, so ignore everything else 
+def get_server_cert_from_protocol(proto_data): 
+
+	(b1,b2,b3) = struct.unpack("!BBB",proto_data[0:3])
+	cert_len = (b1 << 16) | (b2 << 8) | b3
+	cert = proto_data[3: 3 + cert_len]
+	m = hashlib.md5() 
+	m.update(cert)
+	fp = ""
+	digest_raw = m.digest()
+	for i in range(len(digest_raw)):
+		fp += binascii.b2a_hex(digest_raw[i]) + ":"
+	return fp[:-1] 
+
+
+if len(sys.argv) != 3 and len(sys.argv) != 2:
+	print >> sys.stderr, "ERROR: usage: <service-id> [notary-db-file>]"
 	exit(1)
 
 service_id = sys.argv[1]
@@ -53,54 +93,24 @@ try:
 	# this is just a hex-representation of a valid client hello message
 	client_hello = binascii.a2b_hex("""8077010301004e0000002000003900003800003500001600001300000a0700c000003300003200002f0300800000050000040100800000150000120000090600400000140000110000080000060400800000030200800000ff9c82ce1e4bc89df2c726b7cebe211ef80a611945d140834eede5674b597be487""") 
 	sock.send(client_hello)
-	server_hello_start = read_data(sock,5)
-	if len(server_hello_start) != 5: 
-		print "Error: unable to read start server hello"
-		exit(1) 
 	
-	print binascii.b2a_hex(server_hello_start)
-	(msg_type, ssl_version, tls_version, rec_length) = struct.unpack('!BBBH',server_hello_start)
-	print "server hello: msg = %s  length = %s" % (msg_type, rec_length)
-	rest_of_server_hello_msg = read_data(sock,rec_length)
-	if len(rest_of_server_hello_msg) != rec_length: 
-		print "Error: unable to read full server hello message"
-		exit(1) 
+	done = False
+	while not done: 
+		t,rec_data = read_record(sock)
+		if t == 22: # handshake message
+			all_hs_protos = get_all_handshake_protocols(rec_data) 
+			for p in all_hs_protos: 
+				if p[0] == 11: 
+					# server certificate message
+					fp = get_server_cert_from_protocol(p[1])
+					if len(sys.argv) == 3: 
+						notary_common.report_observation(sys.argv[2], service_id, fp) 
+					else: 
+						print "INFO: no database specified, not saving observation"
 
-	server_cert_start = read_data(sock,5) 
-	(msg_type, ssl_version, tls_version, rec_length) = struct.unpack('!BBBH',server_cert_start)
-	print "server cert: msg = %s  length = %s" % (msg_type, rec_length)
-	rest_of_server_certificate_msg = read_data(sock,rec_length)
-	if len(rest_of_server_certificate_msg) != rec_length: 
-		print "Error: unable to read full server certificate message"
-		exit(1)
-	
-	cert_content = rest_of_server_certificate_msg[7:]
-	while True: 
-		(b1,b2,b3) = struct.unpack("!BBB",cert_content[0:3])
-		cert_len = (b1 << 16) | (b2 << 8) | b3
-		end = 3 + cert_len
-		cert = cert_content[3:end]
-		#print "cert base64: %s" % base64.b64encode(cert) 
-		m = hashlib.md5() 
-		m.update(cert)
-		fp = ""
-		digest_raw = m.digest()
-		print "hex = %s" % binascii.b2a_hex(digest_raw)
-		for i in range(len(digest_raw)):
-			fp += binascii.b2a_hex(digest_raw[i]) + ":"
-		fp = fp[:-1] 
-
-		# first cert if server's cert according to RCF 2246
-		# ignore the rest of the cert chain for now
-		break
-		#cert_content = cert_content[end:] 
- 		#if len(cert_content) == 0: 
-		#	break
-
-	notary_common.report_observation(sys.argv[2], service_id, fp) 
-
-	print "Successful scan complete: '%s' has key '%s' " % (service_id,fp)
-	
+					print "Successful scan complete: '%s' has key '%s' " % (service_id,fp)
+					done = True
+					break
 	
 except: 
 	traceback.print_exc(file=sys.stdout)
