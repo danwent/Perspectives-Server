eab66e8063bfe9f110c202b184c79d98a94e126e
Dan Wendlandt <danwent@gmail.com>
2011-02-26 18:33:05 +0000

    split svg library from the example client that uses it

--- utilities/generate_svg.py
+++ utilities/generate_svg.py
@@ -121,30 +121,3 @@ def get_svg_graph(service_id, server_result_list, len_days,cur_secs):
 	return res
 
 
-import time
-import sys
-import traceback  
-from client_common import verify_notary_signature, print_notary_reply,fetch_notary_xml, parse_http_notary_list
-
-if len(sys.argv) != 4: 
-	print "usage: %s <service-id> <notary-list-file> <len-days>" % sys.argv[0]
-	exit(1)  
-
-
-sid = sys.argv[1]
-server_list = parse_http_notary_list(sys.argv[2]) 
-
-for s in server_list: 
-	try: 
-		s["results"] = None
-		server = s["host"].split(":")[0]
-		port = s["host"].split(":")[1]
-		code, xml_text = fetch_notary_xml(server,int(port), sid)
-		if code == 200 and verify_notary_signature(sid, xml_text, s["public_key"]):
-			s["results"] = xml_text
-			
-	except Exception, e:
-		pass
-
-print get_svg_graph(sid, server_list, int(sys.argv[3]), time.time())
-
