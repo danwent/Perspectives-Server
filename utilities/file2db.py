91e6c944614673b2fa9933f1702a0637542d9f52
Dan Wendlandt <danwent@gmail.com>
2011-03-01 21:42:08 -0800

    break generic file analyzer into a separate python lib

--- utilities/file2db.py
+++ utilities/file2db.py
@@ -19,50 +19,13 @@ import os
 import re
 import time 
 import sqlite3
+from file_analyzer import run_analyzer, NotaryFileAnalyzer
 
-# analyzer methods will be called in file order 
-def run_analyzer(fname, analyzer): 
-    f = open(fname, 'r')
-    analyzer.start()
-    while True:
-	# per-host
-	line = f.readline()
-	if len(line) == 0:
-		break
-	assert(line.startswith("Start Host:")) 
-	service_id = line.split("'")[1]
-	analyzer.on_service(service_id) 
-
-	while True:
-		# per-key  
-		line = f.readline()
-		if line.startswith("End Host"):
-			break 
-		# otherwise, must be a key 
-		key_arr = line.split()
-		analyzer.on_key(key_arr[0], key_arr[2]) 
-		
-		while True: 
-			line = f.readline()
-			if len(line.strip()) == 0:
-				# done with key
-				break 
-			line2 = f.readline() 
-			analyzer.on_observation(line.split()[1], 
-						line2.split()[1])
-	 
-	
-    analyzer.end()
-
-
-class SQLiteImportAnalyzer: 
+class SQLiteImportAnalyzer(NotaryFileAnalyzer): 
  	
 	def __init__(self, filename):
 		self.conn = sqlite3.connect(filename)
 		self.num_services = 0
-
-	def start(self): 
-		pass 
 
 	def on_service(self, service_id):
 		self.num_services += 1
---
+++ utilities/file_analyzer.py
@@ -0,0 +1,73 @@
+#   This file is part of the Perspectives Notary Server
+#
+#   Copyright (C) 2011 Dan Wendlandt
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# library that helps you write code that analyzes a notary database dumped
+# in the standard textfile format.  Create a class that sub-classes NotaryFileAnalyzer
+# and call 'run_analyzer' with the name of a database dump file and an instance of the
+# analyzer class
+
+
+class NotaryFileAnalyzer: 
+	
+	def start(self): 
+		pass 
+
+	def on_service(self, service_id):
+		pass
+
+	def on_key(self, key_type, key_hash): 
+		pass 
+
+	def on_observation(self, start_ts,end_ts): 
+		pass 
+	
+	def end(self): 
+		pass
+
+# analyzer methods will be called in file order 
+def run_analyzer(fname, analyzer): 
+    f = open(fname, 'r')
+    analyzer.start()
+    while True:
+	# per-host
+	line = f.readline()
+	if len(line) == 0:
+		break
+	assert(line.startswith("Start Host:")) 
+	service_id = line.split("'")[1]
+	analyzer.on_service(service_id) 
+
+	while True:
+		# per-key  
+		line = f.readline()
+		if line.startswith("End Host"):
+			break 
+		# otherwise, must be a key 
+		key_arr = line.split()
+		analyzer.on_key(key_arr[0], key_arr[2]) 
+		
+		while True: 
+			line = f.readline()
+			if len(line.strip()) == 0:
+				# done with key
+				break 
+			line2 = f.readline() 
+			analyzer.on_observation(line.split()[1], 
+						line2.split()[1])
+	 
+	
+    analyzer.end()
+
