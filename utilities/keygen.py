0aa945650b259186a3d84cb616f670ea4813b6a5
Dave Schaefer <dave.schaefer@gmail.com>
2012-07-14 14:16:58 -0700

    keygen - Convert keygen code to python so its easier to run. Parse arguments using argparse.
    Add some notes about *why* we use keylength of 1369.

---
+++ utilities/keygen.py
@@ -0,0 +1,74 @@
+#   This file is part of the Perspectives Notary Server
+#
+#   Copyright (C) 2011 Dan Wendlandt
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, version 3 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Generate a private and public RSA keypair."""
+
+import os
+import re
+import argparse
+import stat
+from subprocess import call
+
+
+# Notary signatures covering observed key data do not need long-term security,
+# as signatures are recomputed frequently and notary keys are easily updated.
+# We want the shortest key size (maximize performance) that is still secure.
+# 1369-bit RSA is deemed secure enough for now [14].
+# For more see the Perspectives academic paper
+# http://perspectivessecurity.files.wordpress.com/2011/07/perspectives_usenix08.pdf
+NEW_KEY_LENGTH = 1369
+DEFAULT_KEY_NAME = "notary.priv"
+
+
+def get_keynames(private_key_name=DEFAULT_KEY_NAME):
+	"""Calculate the names to use for public and private keys."""
+	real_priv_name = private_key_name + ".priv"
+	real_pub_name = private_key_name + ".pub"
+
+	keypat = re.compile("(.*)\.priv$")
+	if (keypat.match(private_key_name)):
+		# don't append ".priv" twice
+		real_priv_name = keypat.match(private_key_name).group(1) + ".priv"
+		real_pub_name = keypat.match(private_key_name).group(1) + ".pub"
+
+	return (real_pub_name, real_priv_name)
+
+
+def generate_keypair(private_key_name=DEFAULT_KEY_NAME):
+	"""Generate a private and public RSA keypair."""
+
+	(real_pub_name, real_priv_name) = get_keynames(private_key_name)
+
+	if not (os.path.isfile(real_priv_name)):
+		print "Generating notary private key '%s'" % (real_priv_name)
+		ret = call(["openssl", "genrsa", "-out", real_priv_name, str(NEW_KEY_LENGTH)])
+		if (ret == 0):
+			print "Success"
+			os.chmod(real_priv_name, stat.S_IRUSR | stat.S_IXUSR) #600 . Won't affect anything on Windows.
+		print "Generating notary public key '%s'" % (real_pub_name)
+		ret = call(["openssl", "rsa", "-in", real_priv_name, "-out", real_pub_name, "-outform", "PEM", "-pubout"])
+		if (ret == 0):
+			print "Success"
+
+	return (real_pub_name, real_priv_name)
+
+
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('private_key', nargs='?', default=DEFAULT_KEY_NAME,
+	help="File to use as the private key. '.priv' will be appended if necessary. Default: \'%(default)s\'.")
+args = parser.parse_args()
+generate_keypair(args.private_key)
