c8ff95f7f4f126833a300bd3978a64ca8e929168
Dave Schaefer <dave.schaefer@gmail.com>
2012-07-15 11:36:55 -0700

    db2file - Refactor to use argparse and sqlalchemy. We want to add default arguments wherever possible.
    Also remove unneeded imports.

--- notary_util/db2file.py
+++ notary_util/db2file.py
@@ -18,18 +18,48 @@
 Export Observation data from a network notary database.
 """
 
-import sys
-import os
-import re
 import time
+import argparse
+
 from notary_db import ndb
 
-def print_sid_info(sid, key_to_obs): 
+
+DEFAULT_OUTFILE = "-"
+service_type_to_key_type = { "1" : "ssh", "2" : "ssl" } #TODO: put in notary_common
+
+
+def print_long_output(obs):
+	"""Print output in an expanded format, grouping keys for each service."""
+
+	old_sid = None
+	num_sids = 0
+
+	key_to_obs  = {}
+	for (service, key, start, end) in obs:
+		sid = service
+		if old_sid != sid:
+			num_sids += 1
+			if num_sids % 1000 == 0:
+				print "processed %s service-ids" % num_sids
+			if old_sid is not None:
+				print_sid_info(old_sid, key_to_obs)
+			key_to_obs = {}
+			old_sid = sid
+
+		if key not in key_to_obs:
+			key_to_obs[key] = []
+		key_to_obs[key].append((start, end))
+
+		print_sid_info(sid, key_to_obs)
+
+def print_sid_info(sid, key_to_obs):
+	"""Print all of the keys for a single service."""
 	s_type = sid.split(",")[1]
 	
 	if s_type not in service_type_to_key_type:
 		return	
 	
+	print >> output_file, ""
 	print >> output_file, "Start Host: '%s'" % sid
 	key_type_text = service_type_to_key_type[s_type]
 	for key in key_to_obs:
@@ -42,38 +72,41 @@ def print_sid_info(sid, key_to_obs):
 		print >> output_file, ""
 	print >> output_file, "End Host"
 
-service_type_to_key_type = { "1" : "ssh", "2" : "ssl" } 
+def print_tuples(obs):
+	"""Print output in a simple tuple format, one record per line. This makes it easy to import the data somewhere else."""
 
-if len(sys.argv) != 2 and len(sys.argv) != 3: 
-  	print >> sys.stderr, "ERROR: usage: <notary-db-file> [output file]"
-  	exit(1)
+	# note: lines starting with '#' should be ignored by the importer.
 
-output_file = sys.stdout
-if len(sys.argv) == 3: 
-	output_file = open(sys.argv[2],'w')
+	output = [] # use list appending for better performance than string concatenation
+	output.append("# Export of network notary Observations from %s" %args.dbname)
+	output.append("# %s\n" % time.ctime())
+	for (service, key, start, end) in obs:
+		output.append("(%s, %s, %s, %s)" % (service, key, start, end))
 
-ndb = ndb(sys.argv[1])
-obs = ndb.get_observations()
-old_sid = None
-num_sids = 0
-
-key_to_obs  = {} 
-for row in obs:
-	sid = row[0]
-	if old_sid != sid: 
-		num_sids += 1
-		if num_sids % 1000 == 0: 
-			print "processed %s service-ids" % num_sids
-		if old_sid is not None: 
-			print_sid_info(old_sid, key_to_obs)
-		key_to_obs = {}
-		old_sid = sid 
- 
-	key = row[1]
-	if key not in key_to_obs:
-		key_to_obs[key] = []
-	key_to_obs[key].append((row[2],row[3]))
+	output = '\n'.join(output)
+	print >> output_file, output
 
 
-print_sid_info(sid, key_to_obs)
-conn.close()  
+
+parser = argparse.ArgumentParser(parents=[ndb.get_parser()], description=__doc__)
+parser.add_argument('output_file', type=argparse.FileType('w'), nargs='?', default=DEFAULT_OUTFILE,
+			help="File to write data to. Use '-' to write to stdout. Writing to stdout is the default.")
+formats = parser.add_mutually_exclusive_group()
+formats.add_argument('--tuples', '--tup', action='store_true',
+			help=print_tuples.__doc__ + " This is the default.")
+formats.add_argument('--long', action='store_true',
+			help=print_long_output.__doc__)
+
+
+args = parser.parse_args()
+
+# pass ndb the args so it can use any relevant ones from its own parser
+ndb = ndb(args)
+
+output_file = args.output_file
+obs = ndb.get_all_observations()
+
+if (args.long):
+	print_long_output(obs)
+else:
+	print_tuples(obs)
