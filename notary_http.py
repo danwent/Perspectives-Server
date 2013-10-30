bd07dce4a2ad6119e09eb11aafcd7cdf81c8b3e8
Dave Schaefer <dave.schaefer@gmail.com>
2013-10-29 21:36:13 -0600

    notary_http - Move log files to the 'logs' dir
    Let's make all of the modules print logs to one central place

--- notary_http.py
+++ notary_http.py
@@ -15,6 +15,7 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
+import errno
 import os
 import struct
 import sys
@@ -42,6 +43,8 @@ class NotaryHTTPServer:
 	ENV_PORT_KEY_NAME='PORT'
 	STATIC_DIR = "notary_static"
 	STATIC_INDEX = "index.html"
+	LOG_DIR = 'logs'
+	LOG_FILE = 'webserver.log'
 
 	CACHE_EXPIRY = 60 * 60 * 24 # seconds. set this to however frequently you scan services.
 
@@ -109,6 +112,9 @@ class NotaryHTTPServer:
 		elif (args.pycache):
 			self.cache = cache.Pycache(args.pycache)
 
+		if not args.echo_screen:
+			self.create_folder(self.LOG_DIR)
+
 		self.use_sni = args.sni
 		self.create_static_index()
 		self.args = args
@@ -174,6 +180,18 @@ class NotaryHTTPServer:
 		index = os.path.join(self.STATIC_DIR, self.STATIC_INDEX)
 		with open (index, 'w') as i:
 			print >> i, lines
+
+	def create_folder(self, path):
+		"""Create a folder if it doesn't already exist."""
+		# use try/except here to avoid a race condition when checking for existance
+		try:
+			os.makedirs(path)
+		except OSError as e:
+			if (os.path.exists(path) and not os.path.isdir(path)):
+				print >> sys.stderr, "ERROR: Could not create log directory '{0}': a file with that name already exists.".format(path)
+				exit(1)
+			elif e.errno != errno.EEXIST:
+				raise
 
 	def get_xml(self, host, port, service_type):
 		"""Fetch the xml response for a given service."""
@@ -400,7 +418,7 @@ cherrypy.config.update({ 'server.socket_port' : notary.web_port,
 			 'log.access_file' : None,
 			 'server.log_request_headers': False,
 			 # end of privacy settings
-			 'log.error_file' : 'error.log', 
+			 'log.error_file' : '{0}/{1}'.format(notary.LOG_DIR, notary.LOG_FILE),
 			 'log.screen' : False } ) 
 
 if (notary.args.echo_screen):
