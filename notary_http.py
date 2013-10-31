ddc03aa74b7e1d6dd6238171b9923bb16e550b78
Dave Schaefer <dave.schaefer@gmail.com>
2013-10-30 19:26:17 -0600

    notary_http - Increment version number while working on changes

--- notary_http.py
+++ notary_http.py
@@ -38,7 +38,7 @@ class NotaryHTTPServer:
 	Collect and share information on website certificates from around the internet.
 	"""
 
-	VERSION = "3.2"
+	VERSION = "3.3a"
 	DEFAULT_WEB_PORT=8080
 	ENV_PORT_KEY_NAME='PORT'
 	STATIC_DIR = "notary_static"
