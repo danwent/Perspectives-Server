a34066e35641a7e8dc5096d31461721b35568ce7
Dave Schaefer <dave.schaefer@gmail.com>
2013-11-05 22:28:42 -0700

    admin - Add crontab jobs without using temp file
    One less thing for the user to worry about

--- admin/setup.sh
+++ admin/setup.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $dir/_common_functions.sh
+# perform intial setup for a notary server
 
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$dir"/_common_functions.sh
+
+# create log directory
 do_setup
 
-# on launch the database and key pair are automatically created if necessary
+# the database and key pair are automatically created by the notary if necessary
 
-# setup crontab
-crontab crontab_content
+# setup cronjobs
+add_cron_job()
+{
+	time_string=$1
+	command_string=$2
 
+	(crontab -l ; echo "$time_string $command_string" ) | crontab -
+}
+
+replace_cron_job()
+{
+	time_string=$1
+	command_string=$2
+
+	# keep all lines that don't match this command,
+	# so the command is updated/replaced if called multiple times
+	# (i.e. we won't add duplicates)
+	( crontab -l | grep -v "$command_string" ) | crontab -
+	add_cron_job "$time_string" "$command_string"
+}
+
+replace_cron_job "5 1,13 * * *" "$dir/start_scan.sh"
+replace_cron_job "@reboot" "$dir/start_webserver.sh"
+add_cron_job "*/5 * * * *" "$dir/start_webserver.sh"
